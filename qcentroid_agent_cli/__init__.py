4c5207bcea5bd22a65655b53a0467eeaba11d35a
mrestivill <mrodriguezestivill@gmail.com>
2024-01-21 15:37:39 +0100

    Update __init__.py

--- qcentroid_agent_cli/__init__.py
+++ qcentroid_agent_cli/__init__.py
@@ -3,7 +3,7 @@ import requests
 import json
 import os
 import mimetypes
-from qcentroid_agent_cli.model.StatusEntity import Status, StatusEntity
+from qcentroid_agent_cli.model import Status, StatusEntity
 
 import ssl
 
